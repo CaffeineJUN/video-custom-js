69491f4528ee19d2a012de1b6b39999bdd891d99
강혁준 <gurwns232@naver.com>
2022-10-06 19:44:44 +0900

    time percent js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const container = document.querySelector('.container')
 const mainVideo = container.querySelector('video')
+const progressBar = container.querySelector('.progress-bar')
+const skipBackward = document.querySelector('.skip-backward i')
+const skipForward = document.querySelector('.skip-forward i')
 const playPauseBtn = document.querySelector('.play-pause i')
+
+mainVideo.addEventListener('timeupdate', e => {
+    let {currentTime, duration} = e.target
+    let percent = (currentTime / duration) * 100
+    progressBar.style.width = `${percent}%`
+})
+
+skipBackward.addEventListener('click', () => {
+    mainVideo.currentTime -= 5
+})
+
+skipForward.addEventListener('click', () => {
+    mainVideo.currentTime += 5
+})
 
 playPauseBtn.addEventListener('click', () => {
     mainVideo.paused ? mainVideo.play() : mainVideo.pause()
